7545dd93575d8dd0916e3136d69683bd60f45906
adumbidiot <nathaniel.daniel23@outlook.com>
2017-03-30 18:59:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello world!');
+const express = require('express');
+const app = express();
+
+app.use(function(req, res, next){
+  console.log(req.path);
+  next();
+});
+
+app.get('/', function(req, res){
+  res.send('Hello from docker!');
+});
+
+app.listen('8080');
