93119593bb2103c8f5baea6b5fd676a2acddbfb9
adumbidiot <nathaniel.daniel23@outlook.com>
2017-03-31 14:40:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const hbar = require('express-handlebars');
 const app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.use(function(req, res, next){
   console.log(req.path);
